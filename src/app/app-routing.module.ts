9c341f5d03ae522c8ca5180f40cc8944cc3dc48f
Alfredo Santoro <alfredosantoro.informatico@gmail.com>
2021-09-30 16:35:36 +0200

    wip: adding sign-in and sign-up pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,20 @@
 import {NgModule} from '@angular/core';
 import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
+import {SignUpComponent} from "./component/sign-up/sign-up.component";
+import {HomeComponent} from "./component/home/home/home.component";
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/Inbox',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
     path: 'folder/:id',
     loadChildren: () => import('./component/folder/folder.module').then(m => m.FolderPageModule)
-  }
+  },
+  { path: 'sign-up', component: SignUpComponent },
+  { path: 'home', component: HomeComponent }
 ];
 
 @NgModule({
